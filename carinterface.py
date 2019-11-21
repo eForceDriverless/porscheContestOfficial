16f8742a5a38d3ea4117088c378bc13d870b0d86
Jakub Zeman <kubaxzeman@gmail.com>
2019-11-21 14:07:37 +0100

    CI

--- carinterface.py
+++ carinterface.py
@@ -8,9 +8,10 @@ import sys
 
 MIN_ANGLE = 60
 MAX_ANGLE = 140
-STEERING_CORRECTION=-10
 
-SPEED = 300
+MAX_SPEED = 1000
+DIR_FORWARD=0
+DIR_BACKWARD=1
 
 # Motor supply enable
 MOTOR_SPL_EN_GPIO = 10
@@ -37,8 +38,13 @@ LED_GPIO = 4
 # Switch GPIO
 SW_GPIO = 26
 
-def Car_Init():
-	# Setup GPIOs
+def motorInit():
+    wiringpi.pwmWrite(MOTOR_PWM_GPIO, 0)
+    wiringpi.digitalWrite(MOTOR_SPL_EN_GPIO, 1)
+    wiringpi.digitalWrite(MOTOR_DISABLE_GPIO, 0)
+
+def gpioInit():
+    # Setup GPIOs
     wiringpi.wiringPiSetupGpio()
     
     wiringpi.pinMode(LED_GPIO, wiringpi.GPIO.OUTPUT)
@@ -60,22 +66,45 @@ def Car_Init():
     wiringpi.pinMode(SONIC_TRIG_GPIO, wiringpi.GPIO.OUTPUT)
     
     GPIO.setmode(GPIO.BCM)
-	# GPIO.setup(SW_GPIO, GPIO.IN)
-	#GPIO.add_event_detect(SW_GPIO, GPIO.FALLING, button_pressed, 200)
-	
 
-def Car_Enable():
-	wiringpi.pwmWrite(MOTOR_PWM_GPIO, 0)
-    wiringpi.digitalWrite(MOTOR_SPL_EN_GPIO, 1)
-    wiringpi.digitalWrite(MOTOR_DISABLE_GPIO, 0)
+def steer(angle):
+    if(angle<MIN_ANGLE):
+        angle=MIN_ANGLE
+    if(angle>MAX_ANGLE):
+        angle=MAX_ANGLE
+    wiringpi.pwmWrite(SERVO_PWM_GPIO, int(angle))
 
-def Steering(Angle):
-	ANG=Angle + (MAX_ANGLE-MIN_ANGLE)/2+STEERING_CORRECTION
-	if(ANG<MIN_ANGLE):
-		ANG=MIN_ANGLE
-	if(ANG>MAX_ANGLE):
-		ANG=MAX_ANGLE
-	wiringpi.pwmWrite(SERVO_PWM_GPIO, ANG)
+def drive(speedReq):
+    if(speedReq<-1): speedReq=-1
+    else:
+        if(speedReq>1): speedReq=1
+    if(speedReq<0):
+        speedReq=-speedReq
+        wiringpi.digitalWrite(MOTOR_DIR_GPIO, DIR_BACKWARD)
+    else:
+        wiringpi.digitalWrite(MOTOR_DIR_GPIO, DIR_FORWARD)
+
+    wiringpi.pwmWrite(MOTOR_PWM_GPIO,int(MAX_SPEED*speedReq))
 	
-def US_Get():
-	
+def getDistanceUS():
+    wiringpi.digitalWrite(SONIC_TRIG_GPIO, 1)
+    time.sleep(0.00001)
+    wiringpi.digitalWrite(SONIC_TRIG_GPIO, 0)
+    
+    # save StartTime
+    StartTime = time.time()
+    while wiringpi.digitalRead(SONIC_ECHO_GPIO) == 0:
+        StartTime = time.time()
+
+    timeOut = StartTime+0.005
+    StopTime = time.time()
+    while wiringpi.digitalRead(SONIC_ECHO_GPIO) == 1:
+        if(time.time()>timeOut): return -1
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+    return distance
--- test.py
+++ test.py
@@ -1,20 +1,35 @@
 import carinterface as CI
+import time
 
 CI.gpioInit()
 CI.motorInit()
 
+CI.drive(0.2)
+
+MIN_ANGLE = 60
+MAX_ANGLE = 140
+
+print("going right")
 for angle in range(MIN_ANGLE, MAX_ANGLE, 1):
     CI.steer(angle)
     time.sleep(0.05)
     
+print("going left")
 for angle in range(MAX_ANGLE, MIN_ANGLE, -1):
     CI.steer(angle)
     time.sleep(0.05)
 
+print("center")
 CI.steer(90)
-while(1):
-	if(CI.getDistanceUS()>20):
-		carinterface.drive(0.3)
-	else:
-		carinterface.drive(0)
-	
+
+print("sonic test")
+timeOut = time.time()+10
+while(time.time()<timeOut):
+    distance = CI.getDistanceUS()
+    print(distance)
+    if(distance>20 or distance==-1):
+        CI.drive(0.3)
+    else:
+        CI.drive(0)
+    time.sleep(0.5)
+CI.drive(0)
