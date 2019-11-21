2da14a47133b1ab8fee22e2cbb2bc69c29c50adb
Jakub Zeman <kubaxzeman@gmail.com>
2019-11-21 10:31:51 +0100

    example

---
+++ library_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+
+sudo apt -y install python3-opencv python3-pip python3-numpy
+sudo apt -y install picamera
+sudo apt -y install python3-picamera
+sudo pip3 install wiringpi
+
