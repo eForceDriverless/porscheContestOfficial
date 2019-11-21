bff66126c19070f3b9da4a232884f2ce96202da3
Tomas Roun <tomas.roun@seznam.cz>
2019-11-21 11:44:03 +0100

    heading

--- heading.py
+++ heading.py
@@ -1,3 +1,5 @@
+import math
+
 def get_heading(lane_lines):
     if len(lane_lines) >= 2:
         _, _, left_x2, _ = lane_lines[0][0]
@@ -9,3 +11,130 @@ def get_heading(lane_lines):
         x1, _, x2, _ = lane_lines[0][0]
         x_offset = x2 - x1
         y_offset = int(height / 2)
+
+
+
+def average_slope_intercept(frame, line_segments):
+    """
+    This function combines line segments into one or two lane lines
+    If all line slopes are < 0: then we only have detected left lane
+    If all line slopes are > 0: then we only have detected right lane
+    """
+    lane_lines = []
+    if line_segments is None:
+        logging.info('No line_segment segments detected')
+        return lane_lines
+
+    height, width, _ = frame.shape
+    left_fit = []
+    right_fit = []
+
+    boundary = 1 / 3
+    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+    right_region_boundary = width * boundary  # right lane line segment should be on left 2/3 of the screen
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2:
+                logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                if x1 < left_region_boundary and x2 < left_region_boundary:
+                    left_fit.append((slope, intercept))
+            else:
+                if x1 > right_region_boundary and x2 > right_region_boundary:
+                    right_fit.append((slope, intercept))
+
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    right_fit_average = np.average(right_fit, axis=0)
+    if len(right_fit) > 0:
+        lane_lines.append(make_points(frame, right_fit_average))
+
+    #logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+
+    return lane_lines
+
+
+def compute_steering_angle(frame, lane_lines):
+    """ Find the steering angle based on lane line coordinate
+        We assume that camera is calibrated to point to dead center
+    """
+    if len(lane_lines) == 0:
+        logging.info('No lane lines detected, do nothing')
+        return -90
+
+    height, width, _ = frame.shape
+    if len(lane_lines) == 1:
+        logging.debug('Only detected one lane line, just follow it. %s' % lane_lines[0])
+        x1, _, x2, _ = lane_lines[0][0]
+        x_offset = x2 - x1
+    else:
+        _, _, left_x2, _ = lane_lines[0][0]
+        _, _, right_x2, _ = lane_lines[1][0]
+        camera_mid_offset_percent = 0.02  # 0.0 means car pointing to center, -0.03: car is centered to left, +0.03 means car pointing to right
+        mid = int(width / 2 * (1 + camera_mid_offset_percent))
+        x_offset = (left_x2 + right_x2) / 2 - mid
+
+    # find the steering angle, which is angle between navigation direction to end of center line
+    y_offset = int(height / 2)
+
+    angle_to_mid_radian = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
+    angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  # angle (in degrees) to center vertical line
+    steering_angle = angle_to_mid_deg + 90  # this is the steering angle needed by picar front wheel
+
+    #logging.debug('new steering angle: %s' % steering_angle)
+    return steering_angle
+
+
+def stabilize_steering_angle(curr_steering_angle, new_steering_angle, num_of_lane_lines,
+                             max_angle_deviation_two_lines=5, max_angle_deviation_one_lane=1):
+    """
+    Using last steering angle to stabilize the steering angle
+    This can be improved to use last N angles, etc
+    if new angle is too different from current angle, only turn by max_angle_deviation degrees
+    """
+    if num_of_lane_lines == 2:
+        # if both lane lines detected, then we can deviate more
+        max_angle_deviation = max_angle_deviation_two_lines
+    else:
+        # if only one lane detected, don't deviate too much
+        max_angle_deviation = max_angle_deviation_one_lane
+
+    angle_deviation = new_steering_angle - curr_steering_angle
+    if abs(angle_deviation) > max_angle_deviation:
+        stabilized_steering_angle = int(curr_steering_angle
+                                        + max_angle_deviation * angle_deviation / abs(angle_deviation))
+    else:
+        stabilized_steering_angle = new_steering_angle
+    #logging.info('Proposed angle: %s, stabilized angle: %s' % (new_steering_angle, stabilized_steering_angle))
+    return stabilized_steering_angle
+
+def display_heading_line(frame, steering_angle, line_color=(0, 0, 255), line_width=5, ):
+    heading_image = np.zeros_like(frame)
+    height, width, _ = frame.shape
+
+    # figure out the heading line from steering angle
+    # heading line (x1,y1) is always center bottom of the screen
+    # (x2, y2) requires a bit of trigonometry
+
+    # Note: the steering angle of:
+    # 0-89 degree: turn left
+    # 90 degree: going straight
+    # 91-180 degree: turn right
+    steering_angle_radian = steering_angle / 180.0 * math.pi
+    x1 = int(width / 2)
+    y1 = height
+    x2 = int(x1 - height / 2 / math.tan(steering_angle_radian))
+    y2 = int(height / 2)
+
+    cv2.line(heading_image, (x1, y1), (x2, y2), line_color, line_width)
+    heading_image = cv2.addWeighted(frame, 0.8, heading_image, 1, 1)
+
+    return heading_image
+
--- lane_detection.py
+++ lane_detection.py
@@ -114,3 +114,13 @@ def make_points(frame, line):
     x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
     x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
     return [[x1, y1, x2, y2]]
+
+
+def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
+    line_image = np.zeros_like(frame)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
+    line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    return line_image
